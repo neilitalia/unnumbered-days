655458b7b16c89987f3c3480494f74581aaefa80
Neil Italia <neilitalia1998@gmail.com>
2020-01-31 14:30:50 -0600

    :sparkles: Created splash screen

---
+++ src/components/Logo.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Logo.scss'
+import icon from '../svg/today-24px.svg';
+
+function Logo() {
+  return (
+    <div className="Logo">
+      <div className="app-icon">
+        <img src={icon} alt="calendar icon"/>
+      </div>
+      <h1 className="unnumbered-text">unnumbered</h1>
+      <h1 className="days-text">days</h1>
+    </div>
+  )
+}
+
+export default Logo;
---
+++ src/components/SplashScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from './Logo'
+import './SplashScreen.scss'
+
+function SplashScreen() {
+  return (
+    <div className="SplashScreen">
+      <Logo/>
+    </div>
+  )
+}
+
+export default SplashScreen;
